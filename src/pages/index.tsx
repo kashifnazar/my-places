dea3662a59e063f102a15736f5a04ca8dde35616
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-01-23 23:57:21 +0000

    Added profile images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
+import Image from 'next/image'
 
 
 export interface IIndexProps {
@@ -11,14 +12,15 @@ export default function Index (props: IIndexProps) {
       <h1>
           Index page
       </h1>
-      <h1 className="title">
+      <Image src="/images/profile.jpg" alt='Profile' width={144} height={144} />
+      <p className="title">
         Here is the list of the cities I have been to:
           <ul>
             <li><a href="/places/london">London</a></li>
             <li><Link href="/places/islamabad">Islamabad</Link></li>
             <li><Link href="/places/dubai">Dubai</Link></li>
           </ul>
-      </h1>
+      </p>
     </div>
   );
 }
